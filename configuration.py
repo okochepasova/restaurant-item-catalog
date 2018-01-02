8e4d262bf4ded817ecc49f00a9f1cdcbcbe679d6
Olga <kochepasovaolga@gmail.com>
2018-01-02 00:50:40 +0000

    Seperated code works both locally an on a server.

--- configuration.py
+++ configuration.py
@@ -4,5 +4,6 @@ secret_key = 'super_secret_key'
 engine_url = 'sqlite:///restaurantmenuwithusers.db'
 
 def setup_lightsail():
+    global secrets_path, engine_url
     secrets_path = '/home/grader/client_secrets.json'
     engine_url = 'postgresql://grader:udacity@localhost:5432/grader'
